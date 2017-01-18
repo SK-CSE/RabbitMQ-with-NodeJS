4b4f8dde00e109634e9559ff9515d4a50a1f214b
SK-CSE <saurabh.cse04@gmail.com>
2017-01-18 21:24:42 +0530

    #1 Hello world

---
+++ receive.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+// Hello world receiver
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  	conn.createChannel(function(err, ch) {
+    	var q = 'hello';
+
+	    ch.assertQueue(q, {durable: false});
+	   	console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+	   	
+		ch.consume(q, function(msg) {
+		  	console.log(" [x] Received %s", msg.content.toString());
+		}, {noAck: true});
+	});
+});
