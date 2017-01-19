224051d4ad4525ebafb9facf72515360d092e171
SK-CSE <saurabh.cse04@gmail.com>
2017-01-19 22:28:56 +0530

    #4 Routing

--- receive_logs_direct.js
+++ receive_logs_direct.js
@@ -12,21 +12,21 @@ if (args.length == 0) {
 }
 
 amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var ex = 'direct_logs';
 
-    ch.assertExchange(ex, 'direct', {durable: false});
+    conn.createChannel(function(err, ch) {
+        var ex = 'direct_logs';
+        ch.assertExchange(ex, 'direct', {durable: false});
 
-    ch.assertQueue('', {exclusive: true}, function(err, q) {
-      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+        ch.assertQueue('', {exclusive: true}, function(err, q) {
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
 
-      args.forEach(function(severity) {
-        ch.bindQueue(q.queue, ex, severity);
-      });
+            args.forEach(function(severity) {
+              ch.bindQueue(q.queue, ex, severity);
+            });
 
-      ch.consume(q.queue, function(msg) {
-        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
-      }, {noAck: true});
-    });
-  });
+            ch.consume(q.queue, function(msg) {
+                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+            }, {noAck: true});
+        }); 
+    }); 
 });
